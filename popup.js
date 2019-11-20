d809bfdd2e2c36b55bf63374f4a0d037cb09fef9
cenodis <57576911+cenodis@users.noreply.github.com>
2019-11-20 14:34:46 +0100

    Add initial support for First Party Isolation

--- popup.js
+++ popup.js
@@ -39,8 +39,35 @@ function formatActiveTabCookies() {
 	return getActiveTab()
 	.then((tabs) => {
 		const tab = tabs.pop()
-		return browser.cookies.getAll({url: tab.url})
-		.then(formatCookieFile)
+		return Promise.all([Promise.resolve(tab.url), getFirstPartyDomain(tab.url)])
+		.then((results) => {
+			return browser.cookies.getAll({
+				url: results[0],
+				firstPartyDomain: results[1]
+			})
+			.then(formatCookieFile)
+		})
+	})
+}
+
+function isFirstPartyIsolated() {
+	return browser.privacy.websites.firstPartyIsolate.get({}).then((result) => result.value)
+}
+
+/* This function is inteded to return the actual first Party Domain if FPI is enabled and "" otherwise. 
+ * Since there are some difficulties exttacting the FP-Domain form the url this method returns null if
+ * FPI is enabled. When an actual fix is available either edit or replace this method.
+ * 
+ * See also: https://github.com/emersion/ganbo/issues/1
+*/
+function getFirstPartyDomain(url) {
+	return isFirstPartyIsolated()
+	.then((isolated) => {
+		if (isolated) {
+			return null
+		} else {
+			return ""
+		}
 	})
 }
 
@@ -50,8 +77,18 @@ formatActiveTabCookies()
 	document.getElementById('cookies').innerText = cookiesText
 })
 
-browser.cookies.getAll({})
-.then(formatCookieFile)
-.then((cookiesText) => {
-	document.getElementById('download-all').href = 'data:text/plain,' + encodeURIComponent(cookiesText)
+/* Only enable cookies for all domains when FPI is disabled */
+isFirstPartyIsolated()
+.then((isolated) => {
+	if (isolated) {
+		document.getElementById('link-placeholder').innerText = '# Downloading cookies for all domains is not available when First Party Isolation is enabled.'
+	} else {
+		document.getElementById('link-placeholder').innerHTML = '# <a href="#" id="download-all"  download="cookies.txt">Click here</a> to download cookies for all domains'
+
+		browser.cookies.getAll({})
+		.then(formatCookieFile)
+		.then((cookiesText) => {
+			document.getElementById('download-all').href = 'data:text/plain,' + encodeURIComponent(cookiesText)
+		})
+	}
 })
